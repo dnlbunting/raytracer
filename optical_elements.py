da53f21af81d59d5c0a65e3827c42d79db65f403
Daniel Bunting <daniel.bunting14@imperial.ic.ac.uk>
2015-10-19 22:27:56 +0100

    - Adds PlanoConvex and BiConvex lens

--- optical_elements.py
+++ optical_elements.py
@@ -7,6 +7,7 @@ from scipy.spatial import ConvexHull
 from mpl_toolkits.mplot3d import Axes3D
 from .import n_air
 eps = np.finfo(np.float64).eps
+air = lambda l : 1.                                
 
 # TODO Add kwargs to draw that pass to Polygon to allow for more complex rendering ie colors for materials
 class OpticalElement(object):
@@ -125,11 +126,48 @@ class CompositeObject(OpticalElement):
 
 class PlanoConvex(CompositeObject):
     """docstring for PlanoConvex"""
-    def __init__(self, centre, height, radius, thickness):
+    def __init__(self, centre, height, R, thickness, ref_index):
         super(PlanoConvex, self).__init__(centre)
         self.height = height
-        self.radius = radius
-        self.thickness = thickness 
+        self.radius = np.linalg.norm(R)
+        self.R = normalise(isVector(R))
+        self.thickness = thickness
+        self.ref_index = ref_index
+        self.w = self.radius - np.sqrt(self.radius**2 - self.height**2)
+        
+        self.add(SphericalRefraction(self.centre- (self.radius - self.w)*self.R,
+                                     self.radius*self.R, height=self.height, n1=air, n2=ref_index))
+        
+        self.add(CylindricalRefraction(self.centre-self.R*self.thickness*0.5,self.R*self.thickness*0.5,
+                                 self.height, air, self.ref_index)) 
+        
+        a = self.height*np.array([0,0,1])
+        b = self.height*normalise(np.cross(a,self.R))
+        self.add(PlaneRefraction(self.centre-self.R*self.thickness,a,b, n1=air, n2=ref_index ))                             
+
+class BiConvex(CompositeObject):
+    """docstring for BiConvex"""
+    def __init__(self, centre, height, R, thickness, ref_index):
+        super(BiConvex, self).__init__(centre)
+        self.height = height
+        self.radius = np.linalg.norm(R)
+        self.R = normalise(isVector(R))
+        self.thickness = thickness
+        self.ref_index = ref_index
+        self.w = self.radius - np.sqrt(self.radius**2 - self.height**2)
+        
+        self.add(SphericalRefraction(self.centre- (self.radius - self.w)*self.R,
+                                     self.radius*self.R, height=self.height, n1=air, n2=ref_index))
+    
+        self.add(CylindricalRefraction(self.centre-self.R*self.thickness*0.5,self.R*self.thickness*0.5,
+                                 self.height, air, self.ref_index)) 
+        
+        self.add(SphericalRefraction(self.centre+ (self.radius - self.w - self.thickness)*self.R,
+                                     -self.radius*self.R, height=self.height, n1=air, n2=ref_index))
+        
+
+
+
 
 class Cylidrical(OpticalElement):
     """docstring for Cylidrical"""
@@ -144,7 +182,7 @@ class Cylidrical(OpticalElement):
                        self.centre + self.L - self.r*self.R,
                        self.centre - self.L - self.r*self.R,
                        self.centre - self.L + self.r*self.R]
-    def drawBench(self, ax):
+    def drawBench(self, ax=None):
         ax = super(Cylidrical, self).drawBench(ax)
         poly = Polygon([x[:2] for x in self.points], True, facecolor = 'none')
         ax.add_patch(poly)
